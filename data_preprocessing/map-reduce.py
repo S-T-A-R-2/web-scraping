82fe791617e2ec8347c32bc566f02a6212d093d7
GeraldlTzy <geraldcalderon016@gmail.com>
2024-11-02 02:08:27 -0600

    map-reduce

--- data_preprocessing/map-reduce.py
+++ data_preprocessing/map-reduce.py
@@ -33,6 +33,44 @@ class Page:
             words[word] = 1
         else:
             words[word] += 1
+    def get_words(self, tag=None):
+        result = []
+        if (not tag):
+            for tag in self.tags_words:
+                for word in self.tags_words[tag]['words']:
+                    result.append(word)
+        return result
+    def contains(self, word):
+        for tag in self.tags_words:
+            if (word in self.tags_words[tag]['words']):
+                return True
+        return False
+
+class WordCollection:
+    def __init__(self):
+        self.words = {}
+
+    def add(self, word, page, tag):
+        if (word not in self.words):
+            self.words[word] = {'Pages': {page: {tag: 1}}}
+        else:
+            temp = self.words[word]
+            if (page not in temp['Pages']):
+                temp['Pages'][page] = {tag: 1}
+            elif (tag not in temp['Pages'][page]):
+                temp['Pages'][page][tag] = 1
+            else:
+                temp['Pages'][page][tag] += 1
+
+    def get_words_pages(self):
+        result = {}
+        for word in self.words:
+            pages = []
+            for page in self.words[word]['Pages']:
+                pages.append(page)
+            result[word] = pages
+        return result
+
 # Connect to MariaDB Platform
 host = '192.168.100.15'
 user = 'frijol'
@@ -66,7 +104,7 @@ lines = content.splitlines()
 current_page = None
 current_tag = 'p'
 open_tag = False
-sep = r'[;,.:()\s]+'
+sep = r'[;,.:()"\s]+'
 pages = []
 page = Page('a')
 tags = ['#', '##', '###', '####', '#####', '######']
@@ -74,6 +112,9 @@ tags = ['#', '##', '###', '####', '#####', '######']
 def has_number(string):
     return bool(re.search(r'\d', string))
 
+
+words = WordCollection()
+words_contiguous = WordCollection()
 # Read text
 for line in lines:
     # Get current page url
@@ -103,7 +144,11 @@ for line in lines:
         elif ((word in discard) or (re.match(r'^\d+$', word)) or (len(word) < 2) or has_number(word)):
             continue
 
-        page.add_word(word, current_tag)
+        #page.add_word(word, current_tag)
+        words.add(word, current_page, current_tag)
+        #if (word not in words):
+        #    print('voma')
+        #    words.append(word)
         # Mapear y reducir las palabras contiguas
         contiguous = line[index+1].lower()
 
@@ -111,7 +156,9 @@ for line in lines:
             continue
 
         word_contiguous = word + " " + contiguous
-        page.add_word(word_contiguous, current_tag)
+        words_contiguous.add(word_contiguous, current_page, current_tag)
+        #page.add_word(word_contiguous, current_tag)
+        #words.add(word, current_page, current_tag)
 
     # Revisar la última palabra que fue excluida para calcular las palabras contiguas
     last = line[-1]
@@ -119,13 +166,65 @@ for line in lines:
         open_tag = not open_tag
         if (not open_tag):
             current_tag = 'p'
-    if last not in discard and (not re.match(r'^\d+$', last)) and len(last) > 1:
-        page.add_word(word, current_tag)
+    if last not in discard and (not re.match(r'^\d+$', last)) and len(last) > 1 and not has_number(contiguous):
+        #page.add_word(last, current_tag)
+        words.add(last, current_page, current_tag)
 
 # Sort the dictionary
 # words = dict(sorted(words.items(), key=lambda item: item[1], reverse=True))
 # words_contiguous = dict(sorted(words_contiguous.items(), key=lambda item: item[1], reverse=True))
-for page in pages[0:1]:
-    print(page.url)
-    print(page.tags_words)
-    print(page.tags_words_contiguous)
+#print(words.words)
+
+#for word in words.words:
+#    for page in words.words[word]['Pages']:
+#        if page == 'https://es.wikipedia.org/wiki/Canal_45_(Chile)':
+#            for tag in words.words[word]['Pages'][page]:
+#                print(f'{word}  tag: {tag}, count: {words.words[word]['Pages'][page][tag]}')
+
+#for word in words_contiguous.words:
+#    for page in words_contiguous.words[word]['Pages']:
+#        if page == 'https://es.wikipedia.org/wiki/Canal_45_(Chile)':
+#            for tag in words_contiguous.words[word]['Pages'][page]:
+#                print(f'{word}  tag: {tag}, count: {words_contiguous.words[word]['Pages'][page][tag]}')
+
+# First hadoop
+# print(words.get_words_pages())
+
+# Second hadoop
+# print(words_contiguous.get_words_pages())
+
+# Third h
+result = {}
+for word in words.words:
+    for page in words.words[word]['Pages']:
+        total = 0
+        for tag in words.words[word]['Pages'][page]:
+            total += words.words[word]['Pages'][page][tag]
+        result[word] = {page: total}
+        #break
+    #break
+print(result)
+
+# Fourth
+for word in words.words:
+    total = 0
+    for page in words.words[word]['Pages']:
+        for tag in words.words[word]['Pages'][page]:
+            total += words.words[word]['Pages'][page][tag]
+        result[word] = {page: total}
+        #break
+print(result)
+
+# fifth
+print(words.words)
+
+# six
+print(words_contiguous)
+
+# seven
+
+
+
+
+
+
