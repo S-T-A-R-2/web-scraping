efeda22290bd773ba27d24d1724508133bdece73
OscarObando <126714980+OscarObando@users.noreply.github.com>
2024-11-16 12:55:33 -0600

    Add files via upload

---
+++ SQLScripts/queries456.sql
@@ -0,0 +1,43 @@
+-- 4
+DELIMITER //
+CREATE PROCEDURE GetWordTotalCount (IN word_text VARCHAR(60))
+BEGIN
+    SELECT SUM(ptw.count) AS total_count
+    FROM PageTagsWords ptw
+    INNER JOIN Word w 
+    ON ptw.id_word = w.id
+    WHERE LOWER(w.text) = LOWER(word_text)
+END //
+DELIMITER ;
+-- 5
+DELIMITER //
+CREATE PROCEDURE GetWordTagCountInPage (IN word_text VARCHAR(60))
+BEGIN
+    SELECT t.text AS tag, p.url AS page_url, SUM(ptw.count) AS count
+    FROM PageTagsWords ptw
+    INNER JOIN Word w 
+    ON ptw.id_word = w.id
+    INNER JOIN Tag t 
+    ON ptw.id_tag = t.id
+    INNER JOIN Page p 
+    ON ptw.id_page = p.id
+    WHERE LOWER(w.text) = LOWER(word_text)
+    GROUP BY t.text, p.url;
+END //
+DELIMITER ;
+-- 6
+DELIMILER //
+CREATE PROCEDURE GetWordContTagCountInPage (IN word_text VARCHAR(60))
+BEGIN
+    SELECT t.text AS tag, p.url AS page_url, SUM(ptw.count) AS count
+    FROM WordContPage wcp
+    INNER JOIN WordContiguous wc 
+    ON wcp.id_word = wc.id
+    INNER JOIN Tag t 
+    ON wcp.id_tag = t.id
+    INNER JOIN Page p 
+    ON wcp.id_page = p.id
+    WHERE LOWER(wc.text) = LOWER(word_text)
+    GROUP BY t.text, p.url;
+END //
+DELIMITER ;
